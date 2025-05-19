9cfd76375d7c035e30da6299788b9b7a22fbd57c
walisson27 <walisson_souza7@hotmail.com>
2025-05-19 01:50:04 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true, 
-  }
+  base: './', // importante para rotas relativas em produção
 });
