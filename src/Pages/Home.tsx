24eec393d7c939c8246add6456fddc050a1ac8c0
walisson27 <walisson_souza7@hotmail.com>
2025-07-18 23:48:50 -0300

    fix: home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,12 +7,11 @@ const Home = () => {
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {productData.map((product) => (
           <li key={product.id} className="border p-4 rounded shadow">
+            <a href={`/ProductPage?id=${product.id}`} >
             <img src={product.images[0]} alt={product.title} className="w-full h-40 object-cover" />
             <h2 className="mt-2 font-semibold">{product.title}</h2>
             <p className="text-green-600 font-bold">R$ {product.price.toFixed(2)}</p>
-            <a href={`/ProductPage?id=${product.id}`} className="text-blue-500 underline mt-2 block">
-              Ver Produto
-            </a>
+          </a>
           </li>
         ))}
       </ul>
