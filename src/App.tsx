a6baf837d43f40477a04026d205205448b06ef1d
walisson27 <walisson_souza7@hotmail.com>
2025-07-18 23:25:08 -0300

    feat: footer creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductPage from "./Pages/ProductPage"
 import Home from "./Pages/Home";
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer.tsx/Footer";
 function App() {
   return (
     <Router>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ProductPage" element={<ProductPage />} />
       </Routes>
+      <Footer/>
     </Router>
   )
 }
---
+++ src/components/Footer.tsx/Footer.tsx
@@ -0,0 +1,13 @@
+import "./footer.css"
+
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
