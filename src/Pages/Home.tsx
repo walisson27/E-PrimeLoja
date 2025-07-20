b5d06105a748eb6ab90ec5e62da68b4e0eb2bbf4
walisson27 <walisson_souza7@hotmail.com>
2025-07-19 22:37:46 -0300

    feat: create onder

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,16 +1,39 @@
+import  { useState } from "react";
 import { productData } from "../productData";
+import Order from "../components/Order/Order";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  images: string[];
+}
+
 
 const Home = () => {
+    const [sortType, setSortType] = useState<string>("az");
+
+  const produtosOrdenados = [...productData].sort((a: Product, b: Product) => {
+    if (sortType === "az") return a.title.localeCompare(b.title);
+    if (sortType === "za") return b.title.localeCompare(a.title);
+    if (sortType === "menorPreco") return a.price - b.price;
+    if (sortType === "maiorPreco") return b.price - a.price;
+    return 0;
+  });
+
   return (
     <div className="max-w-5xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">Escolha um Produto</h1>
+      <header className="flex flex-row justify-between items-center mb-6">
+         <h1 className="text-2xl font-bold mb-4">Escolha um Produto</h1>
+         <Order sortType={sortType} onSortChange={setSortType} />
+      </header>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {productData.map((product) => (
-          <li key={product.id} className="border p-4 rounded shadow">
+        {produtosOrdenados.map((product: Product) => (
+          <li key={product.id} className="border p-0 rounded shadow">
             <a href={`/ProductPage?id=${product.id}`} >
             <img src={product.images[0]} alt={product.title} className="w-full h-40 object-cover" />
-            <h2 className="mt-2 font-semibold">{product.title}</h2>
-            <p className="text-green-600 font-bold">R$ {product.price.toFixed(2)}</p>
+            <h2 className="mt-2 font-semibold text-center">{product.title}</h2>
+            <p className="text-green-600 font-bold text-center">R$ {product.price.toFixed(2)}</p>
           </a>
           </li>
         ))}
---
+++ src/components/Order/Order.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface OrderProps {
+  sortType: string;
+  onSortChange: (value: string) => void;
+}
+
+const Order: React.FC<OrderProps> = ({ sortType, onSortChange }) => {
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <label className="mr-2 font-semibold" htmlFor="sort-select">
+          Ordenar por:
+        </label>
+        <select
+          id="sort-select"
+          value={sortType}
+          onChange={(e) => onSortChange(e.target.value)}
+          className="border rounded px-2 py-1"
+        >
+          <option value="az">A - Z</option>
+          <option value="za">Z - A</option>
+          <option value="menorPreco">Menor Preço</option>
+          <option value="maiorPreco">Maior Preço</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
