4b3858aa37d5f613ea98a3d0f2cd4e280465dea6
walisson27 <walisson_souza7@hotmail.com>
2025-07-23 16:31:48 -0300

    feact: create cadastro

--- src/components/Users/NewUser/Cadastro.tsx
+++ src/components/Users/NewUser/Cadastro.tsx
@@ -1,23 +1,43 @@
+import  { useState } from 'react';
 
 
-
+interface formData {
+    name: string;
+    email: string;
+    address: string;
+    password: string;
+}
 
 const Cadastro = () => {
+const [form, setForm] = useState<formData>({
+    name: '',
+    email: '',
+    address: '',
+    password: ''
+  });
 
-
-
-
-
+const user = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Formulário enviado:", form);
+    setForm({
+      name: '',
+      email: '',
+      address: '',
+      password: ''
+    });
+  }
 
   return (
         <>
-        <form className="max-w-md mx-auto mt-10 p-6 bg-white rounded-2xl shadow-lg space-y-5">
+        <form onSubmit={user} className="max-w-md mx-auto mt-10 p-6 bg-white rounded-2xl shadow-lg space-y-5">
         <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nome</label>
             <input
             type="text"
             id="name"
             name="name"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
             className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 outline-none transition"
             />
         </div>
@@ -28,6 +48,8 @@ const Cadastro = () => {
             type="email"
             id="email"
             name="email"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
             className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 outline-none transition"
             />
         </div>
@@ -38,6 +60,8 @@ const Cadastro = () => {
             type="text"
             id="adress"
             name="adress"
+            value={form.address}
+            onChange={(e) => setForm({ ...form, address: e.target.value })}
             className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 outline-none transition"
             />
         </div>
@@ -48,6 +72,8 @@ const Cadastro = () => {
             type="password"
             id="password"
             name="password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
             className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 outline-none transition"
             />
         </div>
