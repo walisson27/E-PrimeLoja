5ce7efe6aa76da9dcac7d62ea9d22a92f80bfe85
walisson27 <walisson_souza7@hotmail.com>
2025-07-25 23:36:36 -0300

    fix: navbar display flex

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,9 @@ import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">Home</Link>
-      </div>
+    <nav className="navbar ">
       <ul className="navbar-menu">
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/user">User</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
